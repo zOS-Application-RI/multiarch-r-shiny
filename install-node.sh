b5e8d22ad2df6b1edc9ee37aebc363159d6fb453
ashissah <ashissah@in.ibm.com>
2021-11-19 01:54:09 +0530

    s390x Nodejs
    Signed-off-by: ashissah <ashissah@in.ibm.com>

--- install-node.sh
+++ install-node.sh
@@ -5,7 +5,6 @@ set -eux
 # From the relevant SHASUMS256.txt file at:
 # https://github.com/jcheng5/node-centos6/releases
 # The node-v{VERSION}-linux-x64.tar.xz checksum is the one we need.
-NODE_SHA256=80fc80cdb3d829ea4d752c2e52067a426f6c4fd629ecca5a858d268af8d5ec7e
 
 cd $(dirname $0)
 cd ../..
@@ -34,9 +33,9 @@ verify_checksum () {
 
 download_node () {
   case $(uname -m) in
-  s390x ) export archt=s390x
+  s390x ) export archt=s390x && export NODE_SHA256=5f9b580fc0d9cb412c0482ede23de2c68063942fecd44565cc0e509ed06b4d02
   ;;
-  x86_64 ) export archt=x64
+  x86_64 ) export archt=x64 && export NODE_SHA256=80fc80cdb3d829ea4d752c2e52067a426f6c4fd629ecca5a858d268af8d5ec7e
   ;;
   ppc64le ) export archt=ppc64le
   ;;
